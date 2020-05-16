fd35d0a894792c34c5259d0ba3c5648a44142044
Mario Alviano <mario.alviano@gmail.com>
2020-05-17 01:54:30 +0200

    Move main code in a testable file. Add tests for main entrypoint.

---
+++ tests/translators/test_json2python.py
@@ -0,0 +1,117 @@
+import subprocess
+import sys
+from subprocess import Popen, PIPE
+from typing import Tuple, List
+
+import pytest
+
+from valasp.main import main
+
+
+def test_yaml_from_string():
+    # add a method to process yaml from string, and also other portions of the code
+    # should be unit testable as much as possible
+    raise ModuleNotFoundError
+
+
+def test_main_on_no_args(tmp_path):
+    with pytest.raises(SystemExit) as error:
+        main()
+    assert error.value.code == 1
+
+
+def test_main_on_one_arg():
+    with pytest.raises(SystemExit) as error:
+        main()
+    assert error.value.code == 1
+
+
+def test_main_on_three_args():
+    with pytest.raises(SystemExit) as error:
+        main()
+    assert error.value.code == 1
+
+
+def test_main_on_empty_file(tmp_path):
+    inp = tmp_path / "input.yaml"
+    inp.write_text('')
+    sys.argv = [sys.argv[0], inp, tmp_path / "output.yaml"]
+    main()
+    # no exception at this level; we have to inform the user
+
+
+def test_main_on_missing_file(tmp_path):
+    sys.argv = [sys.argv[0], tmp_path / "input.yaml", tmp_path / "output.yaml"]
+    main()
+
+
+def run_main_on_yaml(tmp_path, yaml_content) -> str:
+    inp = tmp_path / "input.yaml"
+    inp.write_text(yaml_content)
+    dest = tmp_path / "output.yaml"
+    sys.argv = [sys.argv[0], inp, dest]
+    main()
+    print (dest.read_text())
+    return str(dest)
+
+
+def run_clingo_as_process(files: List[str]) -> Tuple[str, str, int]:
+    process = Popen(["clingo"] + files, stdout=PIPE, stderr=PIPE)
+    (output, err) = process.communicate()
+    exit_code = process.wait()
+    return output.decode(), err.decode(), exit_code
+
+
+def write_file(tmp_path, filename: str, content: str) -> str:
+    f = tmp_path / filename
+    f.write_text(content)
+    return str(f)
+
+
+def test_main_on_yaml_with_errors(tmp_path):
+    asp_file = run_main_on_yaml(tmp_path, """
+        Foo:
+            bar: int
+    """)
+
+    (out, err, exit_code) = run_clingo_as_process([
+        asp_file,
+        write_file(tmp_path, "testcase.asp", "foo(1).")
+    ])
+    assert exit_code not in [10, 20, 30]
+    assert 'syntax error' in err
+    assert 'parsing failed' in err
+
+
+def test_main_on_yaml_valasp_is_not_reserved(tmp_path):
+    asp_file = run_main_on_yaml(tmp_path, """
+            valasp:
+                bar: int
+        """)
+
+    (out, err, exit_code) = run_clingo_as_process([
+        asp_file,
+        write_file(tmp_path, "testcase.asp", "valasp(wrong).")
+    ])
+    assert exit_code not in [10, 20, 30]
+    assert 'TypeError: expecting clingo.SymbolType.Number, but received' in err
+    
+
+def test_main_on_yaml_file_with_one_atom(tmp_path):
+    asp_file = run_main_on_yaml(tmp_path, """
+        foo:
+            bar: int
+    """)
+
+    (out, err, exit_code) = run_clingo_as_process([
+        asp_file,
+        write_file(tmp_path, "testcase.asp", "foo(wrong).")
+    ])
+    assert exit_code not in [10, 20, 30]
+    assert 'TypeError: expecting clingo.SymbolType.Number, but received' in err
+
+    (out, err, exit_code) = run_clingo_as_process([
+        asp_file,
+        write_file(tmp_path, "testcase.asp", "foo(1).")
+    ])
+    assert exit_code in [10, 20, 30]
--- valasp/__main__.py
+++ valasp/__main__.py
@@ -3,30 +3,7 @@
 # This file is part of ValAsp which is released under the Apache License, Version 2.0.
 # See file README.md for full license details.
 
-import sys
-import yaml
-
-from valasp.translators import input2json, json2python
+from valasp.main import main
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print('Usage %s input_file output_file' % sys.argv[0])
-        exit(1)
-
-    filename = sys.argv[1]
-    values = None
-    with open(filename, 'r') as f:
-        try:
-            yaml.safe_load(f)
-            values = input2json.create_json_structure(filename, 'yaml')
-        except yaml.YAMLError as exc:
-            values = input2json.create_json_structure(filename, 'asp')
-
-    v = json2python.Validation()
-    v.start_validation(values)
-    v.validate()
-    output = v.end_validation()
-
-    destination = sys.argv[2]
-    with open(destination, 'w', encoding='utf-8') as f:
-        f.write(output)
+    main()
