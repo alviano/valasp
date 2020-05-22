03fb65ea7515d775627664407b4b03395534ca98
Mario Alviano <mario.alviano@gmail.com>
2020-05-22 17:00:47 +0200

    Remove dead code. Disable main.

--- valasp/main.py
+++ valasp/main.py
@@ -2,29 +2,26 @@
 # See file README.md for full license details.
 import sys
 
-import yaml
-
-from valasp.translators import input2json, json2python
-
 
 def main():
     if len(sys.argv) != 3:
         print('Usage %s input_file output_file' % sys.argv[0])
         exit(1)
 
-    filename = sys.argv[1]
-    with open(filename, 'r') as f:
-        try:
-            yaml.safe_load(f)
-            values = input2json.create_json_structure(filename, 'yaml')
-        except yaml.YAMLError as exc:
-            values = input2json.create_json_structure(filename, 'asp')
-
-    v = json2python.Validation()
-    v.start_validation(values)
-    v.validate()
-    output = v.end_validation()
-
-    destination = sys.argv[2]
-    with open(destination, 'w', encoding='utf-8') as f:
-        f.write(output)
+    raise ModuleNotFoundError("currently not implemented")
+    # filename = sys.argv[1]
+    # with open(filename, 'r') as f:
+    #     try:
+    #         yaml.safe_load(f)
+    #         values = input2json.create_json_structure(filename, 'yaml')
+    #     except yaml.YAMLError as exc:
+    #         values = input2json.create_json_structure(filename, 'asp')
+    #
+    # v = json2python.Validation()
+    # v.start_validation(values)
+    # v.validate()
+    # output = v.end_validation()
+    #
+    # destination = sys.argv[2]
+    # with open(destination, 'w', encoding='utf-8') as f:
+    #     f.write(output)
