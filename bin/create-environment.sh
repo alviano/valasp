84395972016793c8a3a9a4474ac5b7e8b7ef5367
Mario Alviano <mario.alviano@gmail.com>
2023-07-23 11:57:07 +0200

    fix versions of deps

--- bin/create-environment.sh
+++ bin/create-environment.sh
@@ -8,6 +8,6 @@ fi
 
 conda create --name "$name" python=3.7
 
-conda install --yes --name "$name" pytest
-conda install --yes --name "$name" -c potassco clingo
-conda install --yes --name "$name" pyyaml
+conda install --yes --name "$name" pytest=5.4.1
+conda install --yes --name "$name" -c potassco clingo=5.4.0
+conda install --yes --name "$name" pyyaml=5.3.1
